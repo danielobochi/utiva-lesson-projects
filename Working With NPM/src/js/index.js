74fe423696df86050a1d872a6604153e7ac8e07a
Kenneth Jimmy <kenjimmy17@gmail.com>
2022-05-06 23:27:12 +0100

    npm lecture

---
+++ Working With NPM/src/js/index.js
@@ -0,0 +1,47 @@
+import _ from "lodash";
+import axios from "axios";
+
+const url =
+  "https://polar-dusk-46748.herokuapp.com/https://api.fliplet.com/v1/widgets/assets";
+
+const handleAssets = async () => {
+  let assets;
+
+  try {
+    const response = await axios.get(url);
+    assets = response.data;
+  } catch (err) {
+    console.log(err);
+  }
+
+  console.log(assets);
+};
+
+handleAssets();
+
+/*
+const getFromObj = (obj, target) => {
+    return _.get(obj, [target]);
+  };
+
+  const hasDependencies = (obj) => {
+    return _.has(obj, "dependencies");
+  };
+
+  const getVersions = (obj) => {
+    const versions = [];
+    _.forOwn(obj, function (value, key) {
+      versions.push(value);
+    });
+    return versions;
+  };
+
+  const normalizeAssets = (arrAssets) => {
+    let arr = [...arrAssets];
+
+    arr = _.flattenDeep(arr);
+    arr = _.union(arr);
+
+    return arr;
+  };
+  */
