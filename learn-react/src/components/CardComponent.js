1b4349973064468b69958bc24916ff446dc59930
Kenneth Jimmy <kenjimmy17@gmail.com>
2022-05-19 22:20:19 +0100

    ...

--- learn-react/src/components/CardComponent.js
+++ learn-react/src/components/CardComponent.js
@@ -5,12 +5,19 @@ function CardComponent(props) {
   return (
     <div className="card">
       <div className="title" style={{ background: props.colour }}>
-        <h2>{props.title}</h2>
+        <h2 className="fs-1">{props.title}</h2>
       </div>
 
       <div className="description">
         <p>{props.description}</p>
       </div>
+
+      <button
+        className="btn-secondary w-50 align-self-end"
+        onClick={props.addToCart}
+      >
+        Add to cart
+      </button>
     </div>
   );
 }
